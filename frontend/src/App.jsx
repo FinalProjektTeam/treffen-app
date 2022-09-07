b2a30cf936743b1f0d6bbc0caa5b9ec2ef0f5898
SusanAbo <s.aboismaeel@gmail.com>
2022-09-07 21:20:34 +0200

    create Navi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
 import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 import Home from './components/Home'
 import Navigation from './components/Navigation'
@@ -34,22 +33,6 @@ function App() {
     </div>
     </Router>
     
-=======
-import Home from './components/Home';
-import Login from './components/Login';
-
-function App() {
-  return (
-    <div className="App">
-    
-      <h1>Test</h1>
-
-      <div className="components">
-        <Home />
-        <Login  />
-      </div>
-    </div>
->>>>>>> 7b62de3b824d990f1beaf3700566bb9cb8de4db7
   );
 }
 
