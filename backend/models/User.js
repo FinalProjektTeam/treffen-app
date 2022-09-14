bff0a5b270b50b15eb05795bd2f27a82833e29f2
SusanAbo <s.aboismaeel@gmail.com>
2022-09-14 09:36:15 +0200

    14.09

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,22 +1,14 @@
-const {Schema, model, SchemaTypes} = require('mongoose')
+const {Schema, model} = require('mongoose')
 
 const userSchema = Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     firstname: {type: String, required: true},
     lastname: {type: String, required: true},
-    gender: {
-        type: String, 
-        enum: ['Male', 'Female'],
-        required: true
-    },
+    gender: {type: String, required: true},
     age: {type: String, required: true},
-    avatar: {type: String},
-    events: [{
-        type: SchemaTypes.ObjectId,
-        ref: 'Event'
-    }]
+    avatar: {type: String}
     
 })
 
-module.exports = model('User', userSchema)
+module.exports = model('user', userSchema)
